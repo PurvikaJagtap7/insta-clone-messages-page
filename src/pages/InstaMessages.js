f18672be570f14fd773a72821a9387f16169dbd9
Purvika Jagtap <purvikajagtap@gmail.com>
2024-03-12 14:02:13 +0530

    Add files via upload

---
+++ src/pages/InstaMessages.js
@@ -0,0 +1,370 @@
+const InstaMessages = () => {
+  return (
+    <div className="w-full relative bg-white h-[844px] overflow-hidden text-center text-mini text-black font-segoe-ui">
+      <div className="absolute top-[0px] left-[0px] w-[390px] h-11 overflow-hidden text-gray-200 font-sf-pro-text">
+        <div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%]" />
+        <img
+          className="absolute top-[calc(50%_-_4px)] right-[14.5px] w-[24.5px] h-[10.5px] object-cover"
+          alt=""
+          src="/battery@2x.png"
+        />
+        <img className="relative w-[15.9px] h-[11px]" alt="" src="/wifi.svg" />
+        <img
+          className="relative w-[17.7px] h-[10.7px]"
+          alt=""
+          src="/mobile-signal.svg"
+        />
+        <div className="absolute h-[40.91%] w-[14.41%] top-[31.82%] right-[80%] bottom-[27.27%] left-[5.59%] overflow-hidden">
+          <div className="absolute w-[calc(100%_-_0.2px)] top-[calc(50%_-_9px)] left-[0px] tracking-[-0.3px] font-semibold inline-block">
+            9:41
+          </div>
+        </div>
+      </div>
+      <div className="absolute bottom-[0px] left-[0px] w-[390px] h-[34px] overflow-hidden">
+        <div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%]" />
+        <div className="absolute bottom-[9px] left-[calc(50%_-_67px)] rounded-81xl bg-gray-300 w-[134px] h-[5px]" />
+      </div>
+      <div className="absolute h-[4.5%] top-[91.94%] bottom-[3.55%] left-[0px] bg-white box-border overflow-hidden flex flex-col items-start justify-start pt-1.5 px-[25px] pb-0.5 border-t-[0.5px] border-solid border-gray-500">
+        <div className="flex flex-row items-center justify-start gap-[0px_61px]">
+          <div className="flex flex-row items-start justify-start gap-[0px_57px]">
+            <img
+              className="w-[22px] relative h-[22.7px]"
+              alt=""
+              src="/shape.svg"
+            />
+            <img
+              className="w-[23.4px] relative h-[23.4px]"
+              alt=""
+              src="/shape1.svg"
+            />
+            <img
+              className="w-[23.5px] relative h-[23.5px]"
+              alt=""
+              src="/shape2.svg"
+            />
+          </div>
+          <div className="flex flex-row items-center justify-start gap-[0px_46px]">
+            <img
+              className="w-[22px] relative h-[22px] overflow-hidden shrink-0"
+              alt=""
+              src="/iconsreels.svg"
+            />
+            <img
+              className="w-7 relative rounded-[50%] h-7 object-cover"
+              alt=""
+              src="/ellipse-171@2x.png"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="absolute top-[44px] left-[-5px] flex flex-row items-center justify-start text-base">
+        <div className="h-[45px] flex flex-row items-center justify-center py-2.5 pr-[5px] pl-2.5 box-border gap-[0px_11px]">
+          <img
+            className="w-[46px] relative h-[45px]"
+            alt=""
+            src="/frame-72.svg"
+          />
+          <div className="relative tracking-[-0.3px] font-semibold">itzz_N</div>
+        </div>
+        <img
+          className="w-6 relative h-4 object-cover"
+          alt=""
+          src="/expand-arrow1@2x.png"
+        />
+      </div>
+      <img
+        className="absolute top-[59px] left-[351px] w-[22px] h-[22px] overflow-hidden"
+        alt=""
+        src="/iconsnewmessages.svg"
+      />
+      <img
+        className="absolute top-[54px] left-[298px] w-8 h-[33px] object-cover"
+        alt=""
+        src="/whatsapp-image-20240311-at-713-1@2x.png"
+      />
+      <div className="absolute top-[94px] left-[0px] w-[390px] h-[682px] overflow-y-auto text-left text-xs">
+        <div className="absolute top-[13px] left-[26px] rounded-3xs bg-darkgray-200 w-[342px] overflow-hidden flex flex-col items-start justify-start py-0 px-4 box-border text-center text-sm text-darkgray-100">
+          <div className="flex flex-row items-center justify-start">
+            <img
+              className="w-[17.1px] relative h-[17.1px]"
+              alt=""
+              src="/vector2.svg"
+            />
+            <div className="w-[85px] relative tracking-[-0.3px] flex items-center justify-center h-[34px] shrink-0 ml-[-12px]">
+              Search
+            </div>
+          </div>
+        </div>
+        <div className="absolute top-[79px] left-[44px] flex flex-col items-center justify-start gap-[8px_0px] text-center text-darkgray-100">
+          <div className="flex flex-row items-start justify-start">
+            <img
+              className="w-[79px] relative h-20"
+              alt=""
+              src="/subtract2.svg"
+            />
+          </div>
+          <div className="w-[66px] relative tracking-[-0.3px] flex items-center justify-center h-[11px] shrink-0">
+            Your note
+          </div>
+        </div>
+        <div className="absolute top-[192px] left-[39px] text-sm tracking-[-0.3px] text-center">
+          Messages
+        </div>
+        <div className="absolute top-[192px] left-[324px] text-sm tracking-[-0.3px] text-cornflowerblue text-center">
+          Requests
+        </div>
+        <div className="absolute top-[243px] left-[36px] flex flex-row items-center justify-start gap-[0px_104px]">
+          <div className="flex flex-row items-center justify-start gap-[0px_7px]">
+            <img
+              className="w-[66px] relative rounded-[50%] h-16 object-cover"
+              alt=""
+              src="/ellipse-7@2x.png"
+            />
+            <div className="flex flex-col items-start justify-start">
+              <div className="w-[131px] relative tracking-[-0.3px] inline-block h-[18px] shrink-0">
+                Shreeya jadhav
+              </div>
+              <div className="w-[133px] flex flex-row items-center justify-start relative mt-[-3px] text-center text-darkgray-100">
+                <div className="w-[108px] relative tracking-[-0.3px] inline-block h-[18px] shrink-0 z-[0]">
+                  send a reel by yashna
+                </div>
+                <div className="w-8 relative tracking-[-0.3px] inline-block h-[15px] shrink-0 z-[1]">
+                  2h
+                </div>
+                <div className="w-1 absolute !m-[0] top-[5.5px] left-[111px] tracking-[-0.3px] flex items-center justify-center h-px z-[2]">
+                  .
+                </div>
+              </div>
+            </div>
+          </div>
+          <img
+            className="w-[22px] relative h-[22px]"
+            alt=""
+            src="/vector3.svg"
+          />
+        </div>
+        <div className="absolute top-[327px] left-[39px] flex flex-row items-center justify-start gap-[0px_104px]">
+          <div className="flex flex-row items-center justify-start gap-[0px_7px]">
+            <img
+              className="w-[66px] relative rounded-[50%] h-16 object-cover"
+              alt=""
+              src="/ellipse-71@2x.png"
+            />
+            <div className="flex flex-col items-start justify-start">
+              <div className="w-[131px] relative tracking-[-0.3px] inline-block h-[18px] shrink-0">
+                Ankita Bhoir
+              </div>
+              <div className="w-[133px] flex flex-row items-center justify-start relative mt-[-3px] text-center text-darkgray-100">
+                <div className="w-[108px] relative tracking-[-0.3px] inline-block h-[18px] shrink-0 z-[0]">
+                  send a reel by yashna
+                </div>
+                <div className="w-8 relative tracking-[-0.3px] inline-block h-[15px] shrink-0 z-[1]">
+                  2h
+                </div>
+                <div className="w-1 absolute !m-[0] top-[5.5px] left-[111px] tracking-[-0.3px] flex items-center justify-center h-px z-[2]">
+                  .
+                </div>
+              </div>
+            </div>
+          </div>
+          <img
+            className="w-[22px] relative h-[22px]"
+            alt=""
+            src="/vector3.svg"
+          />
+        </div>
+        <div className="absolute top-[411px] left-[38px] flex flex-row items-center justify-start gap-[0px_104px]">
+          <div className="flex flex-row items-center justify-start gap-[0px_7px]">
+            <img
+              className="w-[66px] relative rounded-[50%] h-16 object-cover"
+              alt=""
+              src="/ellipse-72@2x.png"
+            />
+            <div className="flex flex-col items-start justify-start">
+              <div className="w-[131px] relative tracking-[-0.3px] inline-block h-[18px] shrink-0">
+                Madhura Gujar
+              </div>
+              <div className="w-[133px] flex flex-row items-center justify-start relative mt-[-3px] text-center text-darkgray-100">
+                <div className="w-[108px] relative tracking-[-0.3px] inline-block h-[18px] shrink-0 z-[0]">
+                  send a reel by yashna
+                </div>
+                <div className="w-8 relative tracking-[-0.3px] inline-block h-[15px] shrink-0 z-[1]">
+                  2h
+                </div>
+                <div className="w-1 absolute !m-[0] top-[5.5px] left-[111px] tracking-[-0.3px] flex items-center justify-center h-px z-[2]">
+                  .
+                </div>
+              </div>
+            </div>
+          </div>
+          <img
+            className="w-[22px] relative h-[22px]"
+            alt=""
+            src="/vector3.svg"
+          />
+        </div>
+        <div className="absolute top-[495px] left-[37px] flex flex-row items-center justify-start gap-[0px_104px]">
+          <div className="flex flex-row items-center justify-start gap-[0px_7px]">
+            <img
+              className="w-[66px] relative rounded-[50%] h-16 object-cover"
+              alt=""
+              src="/ellipse-73@2x.png"
+            />
+            <div className="flex flex-col items-start justify-start">
+              <div className="w-[131px] relative tracking-[-0.3px] inline-block h-[18px] shrink-0">
+                Bhoomee Desai
+              </div>
+              <div className="w-[133px] flex flex-row items-center justify-start relative mt-[-3px] text-center text-darkgray-100">
+                <div className="w-[108px] relative tracking-[-0.3px] inline-block h-[18px] shrink-0 z-[0]">
+                  send a reel by yashna
+                </div>
+                <div className="w-8 relative tracking-[-0.3px] inline-block h-[15px] shrink-0 z-[1]">
+                  2h
+                </div>
+                <div className="w-1 absolute !m-[0] top-[5.5px] left-[111px] tracking-[-0.3px] flex items-center justify-center h-px z-[2]">
+                  .
+                </div>
+              </div>
+            </div>
+          </div>
+          <img
+            className="w-[22px] relative h-[22px]"
+            alt=""
+            src="/vector3.svg"
+          />
+        </div>
+        <div className="absolute top-[579px] left-[36px] flex flex-row items-center justify-start gap-[0px_104px]">
+          <div className="flex flex-row items-center justify-start gap-[0px_7px]">
+            <img
+              className="w-[66px] relative rounded-[50%] h-16 object-cover"
+              alt=""
+              src="/ellipse-74@2x.png"
+            />
+            <div className="flex flex-col items-start justify-start">
+              <div className="w-[131px] relative tracking-[-0.3px] inline-block h-[18px] shrink-0">
+                Achal Rane
+              </div>
+              <div className="w-[133px] flex flex-row items-center justify-start relative mt-[-3px] text-center text-darkgray-100">
+                <div className="w-[108px] relative tracking-[-0.3px] inline-block h-[18px] shrink-0 z-[0]">
+                  send a reel by yashna
+                </div>
+                <div className="w-8 relative tracking-[-0.3px] inline-block h-[15px] shrink-0 z-[1]">
+                  2h
+                </div>
+                <div className="w-1 absolute !m-[0] top-[5.5px] left-[111px] tracking-[-0.3px] flex items-center justify-center h-px z-[2]">
+                  .
+                </div>
+              </div>
+            </div>
+          </div>
+          <img
+            className="w-[22px] relative h-[22px]"
+            alt=""
+            src="/vector3.svg"
+          />
+        </div>
+        <div className="absolute top-[663px] left-[35px] flex flex-row items-center justify-start gap-[0px_104px]">
+          <div className="flex flex-row items-center justify-start gap-[0px_7px]">
+            <img
+              className="w-[66px] relative rounded-[50%] h-16 object-cover"
+              alt=""
+              src="/ellipse-75@2x.png"
+            />
+            <div className="flex flex-col items-start justify-start">
+              <div className="w-[131px] relative tracking-[-0.3px] inline-block h-[18px] shrink-0">
+                Nandini Bhoir
+              </div>
+              <div className="w-[133px] flex flex-row items-center justify-start relative mt-[-3px] text-center text-darkgray-100">
+                <div className="w-[108px] relative tracking-[-0.3px] inline-block h-[18px] shrink-0 z-[0]">
+                  send a reel by yashna
+                </div>
+                <div className="w-8 relative tracking-[-0.3px] inline-block h-[15px] shrink-0 z-[1]">
+                  2h
+                </div>
+                <div className="w-1 absolute !m-[0] top-[5.5px] left-[111px] tracking-[-0.3px] flex items-center justify-center h-px z-[2]">
+                  .
+                </div>
+              </div>
+            </div>
+          </div>
+          <img
+            className="w-[22px] relative h-[22px]"
+            alt=""
+            src="/vector3.svg"
+          />
+        </div>
+        <div className="absolute top-[747px] left-[34px] flex flex-row items-center justify-start gap-[0px_104px]">
+          <div className="flex flex-row items-center justify-start gap-[0px_7px]">
+            <img
+              className="w-[66px] relative rounded-[50%] h-16 object-cover"
+              alt=""
+              src="/ellipse-76@2x.png"
+            />
+            <div className="flex flex-col items-start justify-start">
+              <div className="w-[131px] relative tracking-[-0.3px] inline-block h-[18px] shrink-0">
+                Archi Gosavi
+              </div>
+              <div className="w-[133px] flex flex-row items-center justify-start relative mt-[-3px] text-center text-darkgray-100">
+                <div className="w-[108px] relative tracking-[-0.3px] inline-block h-[18px] shrink-0 z-[0]">
+                  send a reel by yashna
+                </div>
+                <div className="w-8 relative tracking-[-0.3px] inline-block h-[15px] shrink-0 z-[1]">
+                  2h
+                </div>
+                <div className="w-1 absolute !m-[0] top-[5.5px] left-[111px] tracking-[-0.3px] flex items-center justify-center h-px z-[2]">
+                  .
+                </div>
+              </div>
+            </div>
+          </div>
+          <img
+            className="w-[22px] relative h-[22px]"
+            alt=""
+            src="/vector3.svg"
+          />
+        </div>
+        <div className="absolute top-[831px] left-[33px] flex flex-row items-center justify-start gap-[0px_104px]">
+          <div className="flex flex-row items-center justify-start gap-[0px_7px]">
+            <img
+              className="w-[66px] relative rounded-[50%] h-16 object-cover"
+              alt=""
+              src="/ellipse-77@2x.png"
+            />
+            <div className="flex flex-col items-start justify-start">
+              <div className="w-[131px] relative tracking-[-0.3px] inline-block h-[18px] shrink-0">
+                Krishnali rane
+              </div>
+              <div className="w-[133px] flex flex-row items-center justify-start relative mt-[-3px] text-center text-darkgray-100">
+                <div className="w-[108px] relative tracking-[-0.3px] inline-block h-[18px] shrink-0 z-[0]">
+                  send a reel by yashna
+                </div>
+                <div className="w-8 relative tracking-[-0.3px] inline-block h-[15px] shrink-0 z-[1]">
+                  2h
+                </div>
+                <div className="w-1 absolute !m-[0] top-[5.5px] left-[111px] tracking-[-0.3px] flex items-center justify-center h-px z-[2]">
+                  .
+                </div>
+              </div>
+            </div>
+          </div>
+          <img
+            className="w-[22px] relative h-[22px]"
+            alt=""
+            src="/vector3.svg"
+          />
+        </div>
+        <div className="absolute top-[61px] left-[34px] shadow-[-4px_0px_4px_rgba(0,_0,_0,_0.25)] w-[60px] h-[41px] text-center text-base text-darkgray-100">
+          <div className="absolute top-[0px] left-[0px] rounded-[15px] bg-white box-border w-[60px] h-[35.8px] overflow-hidden flex flex-row items-center justify-center border-[0.1px] border-solid border-gray-400">
+            <div className="relative tracking-[-0.3px]">Note...</div>
+          </div>
+          <div className="absolute top-[30.5px] left-[12.6px] w-[12.6px] h-[10.5px] flex flex-row items-end justify-start">
+            <div className="w-3 relative rounded-[50%] bg-white h-[11px]" />
+            <div className="w-1 relative rounded-[50%] bg-white h-1" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InstaMessages;
